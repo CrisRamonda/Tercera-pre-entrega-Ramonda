a8aae0193c997a57d5b5e08a4d4777c33c257ec0
Cristian Ramonda <cris.ram94@gmail.com>
2023-04-09 18:47:38 -0300

    funciona css

--- AppHotel/settings.py
+++ AppHotel/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'AppHotel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['D:/Documents/PROGRAMACION/CODERHOUSE/Python/Entregas/3ra Entrega/AppHotel/AppHotel'],
+        'DIRS': ['D:/Documents/PROGRAMACION/CODERHOUSE/Python/Entregas/3ra Entrega/AppHotel/AppHotel/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'AppHotel/static/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- AppHotel/views.py
+++ AppHotel/views.py
@@ -4,6 +4,6 @@ from django.template import loader, Context
 
 def index(request):
     context = {}
-    doc = loader.get_template('static/index.html')
+    doc = loader.get_template('index.html')
     index = doc.render(context)
     return HttpResponse(index)
