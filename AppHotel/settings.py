6c3d5c35f503b1ffc8f9f5ab33bea9e3193febbb
Cristian Ramonda <cris.ram94@gmail.com>
2023-04-09 17:59:44 -0300

    3er plantilla

--- AppHotel/settings.py
+++ AppHotel/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'AppHotel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['D:/Documents/PROGRAMACION/CODERHOUSE/Python/Entregas/3ra Entrega/AppHotel/AppHotel/'],
+        'DIRS': ['D:/Documents/PROGRAMACION/CODERHOUSE/Python/Entregas/3ra Entrega/AppHotel/AppHotel'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- AppHotel/static/js/scripts.js
+++ AppHotel/static/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/template/heroic-features)
+* Start Bootstrap - Landing Page v6.0.6 (https://startbootstrap.com/theme/landing-page)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
