ed42fec1f85b4da75f42ba692d344cc11a36b696
CrisRamonda <71059098+CrisRamonda@users.noreply.github.com>
2023-04-10 23:31:30 -0300

    Delete AppHotel directory

--- AppHotel/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader, Context
-
-
-def index(request):
-    context = {}
-    doc = loader.get_template('index.html')
-    index = doc.render(context)
-    return HttpResponse(index)
