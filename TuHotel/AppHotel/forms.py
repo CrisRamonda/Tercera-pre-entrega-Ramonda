2f6e76be7b5249aa8e75a84486daf70a92655fad
CrisRamonda <71059098+CrisRamonda@users.noreply.github.com>
2023-04-12 01:18:13 -0300

    Delete TuHotel directory

--- TuHotel/AppHotel/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class RegistroCliente(forms.Form):
-
-    nombre = forms.CharField(max_length=100)
-    documento = forms.IntegerField()
-    email = forms.EmailField(max_length=300)
